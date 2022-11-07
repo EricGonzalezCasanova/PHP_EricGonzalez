d62a5cb2e0f419bf07e2ab793267bb0f84fab30f
EricGonzalezCasanova <ericgonzalez@iesebre.com>
2022-11-07 16:58:13 +0100

    Video1

--- app/index.php
+++ app/index.php
@@ -1,3 +1,25 @@
 <?php
 
-$greeting = 'Hola mon!';
+require 'app/helpers.php';
+
+require 'app/Task.php';
+
+//$tasks = new Task(1,'comprar pa','a la panaderia',0);
+//var_dump($tasks);
+
+$user = 'debian-sys-maint';
+$pass = '0oDmPGpFyDlgYVVd';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user, $pass);
+} catch (\Exception $e){
+    echo 'Error de connexió a la base de dades';
+}
+
+$statement = $dbh->prepare('SELECT * FROM tasks');
+
+$statement->execute();
+
+$tasks = $statement->FetchAll(PDO::FETCH_OBJ);
+
+$greeting = greet();
