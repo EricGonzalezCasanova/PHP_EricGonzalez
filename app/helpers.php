b70052301dbc76ba5d971ca9b010715031475bce
EricGonzalezCasanova <ericgonzalez@iesebre.com>
2022-12-05 15:31:14 +0100

    Afegit helper function dd

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,13 @@
 <?php
 function greet() {
 $name = htmlspecialchars($_GET['name']);
-$surname = ($_GET['surname']);
+$surname = $_GET['surname'];
 
 return "Hola $name $surname!";
+}
+
+function dd($xivato)
+{
+    var_dump($xivato);
+    die();
 }
